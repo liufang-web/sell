2ec03047963766504ecd806162b5fef60d9fd7b4
liufang-web <44256700+liufang-web@users.noreply.github.com>
2021-01-12 16:58:56 +0800

    '首页TabBar'

---
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <tabBar>
+    <tabBarItem path="/Home">
+      <template v-slot:item-icon><i class="icon-home3"></i></template>
+      <template v-slot:item-icon-active><i class="icon-home3"></i></template>
+      <template v-slot:item-text><div>首页</div></template>
+    </tabBarItem>
+    <tabBarItem path="/Category">
+      <template v-slot:item-icon><i class="icon-library"></i></template>
+      <template v-slot:item-icon-active><i class="icon-library"></i></template>
+      <template v-slot:item-text><div>分类</div></template>
+    </tabBarItem>
+    <tabBarItem path="/Show">
+      <template v-slot:item-icon><i class="icon-cart"></i></template>
+      <template v-slot:item-icon-active><i class="icon-cart"></i></template>
+      <template v-slot:item-text><div>购物车</div></template>
+    </tabBarItem>
+    <tabBarItem path="/Mine">
+      <template v-slot:item-icon><i class="icon-user"></i></template>
+      <template v-slot:item-icon-active><i class="icon-user"></i></template>
+      <template v-slot:item-text><div>我的</div></template>
+    </tabBarItem>
+  </tabBar>
+</template>
+
+<script>
+  import tabBar from 'components/common/tabBar/TabBar'
+  import tabBarItem from 'components/common/tabBar/TabBarItem'
+  export default {
+    name: "MainTabBar",
+    components: {
+      tabBar,
+      tabBarItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/mine/Mine.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Mine"
+    }
+</script>
+
+<style scoped>
+
+</style>
